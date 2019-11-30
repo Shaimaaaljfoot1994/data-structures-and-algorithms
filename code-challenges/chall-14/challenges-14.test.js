fcb2cee1c914978a8cd37a1e1694f4cdcd243673
shaimaa <Shaimaa.aljfoot@gmil.com>
2019-11-30 13:39:47 +0200

    solu-1

--- code-challenges/chall-14/challenges-14.test.js
+++ code-challenges/chall-14/challenges-14.test.js
@@ -10,6 +10,9 @@ For example, ['apple', 'banana', 'MacGyver'] returns ['Apple', 'Banana', 'MacGyv
 
 const toTitleCase = (arr) => {
   // Solution code here...
+  return arr.map(element => {
+    return element.charAt(0).toUpperCase() + element.substring(1);
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
