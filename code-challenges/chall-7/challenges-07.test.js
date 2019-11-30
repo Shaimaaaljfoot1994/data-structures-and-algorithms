bd420d501717a1438c493184b9883e3e8b619623
shaimaa <Shaimaa.aljfoot@gmil.com>
2019-11-30 12:30:03 +0200

    solu-1

--- code-challenges/chall-7/challenges-07.test.js
+++ code-challenges/chall-7/challenges-07.test.js
@@ -12,6 +12,12 @@ For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and
 
 const forLoopTwoToThe = (arr) => {
   // Solution code here...
+  
+  let arrModi = [];
+  for (let i=0;i<arr.length;i++) {
+    arrModi.push(Math.pow(2,arr[i]));  /// to power of a number 
+  }
+  return arrModi;
 };
 
 /* ------------------------------------------------------------------------------------------------
